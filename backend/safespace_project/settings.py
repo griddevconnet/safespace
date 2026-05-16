52815712d7d2f782b54d7366c49381d7cfa12074
griddevconnet <grid.devconnet@gmail.com>
2026-05-16 13:24:56 +0000

    Add Render URL to ALLOWED_HOSTS default

--- backend/safespace_project/settings.py
+++ backend/safespace_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-gbp3h-d90cf8t++5j72b^maz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'safespace-ybxn.onrender.com'])
 
 
 # Application definition
